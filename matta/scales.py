bb16da088d2565f352f71d35b1f854f511e95e0c
carnby <eduardo.graells@gmail.com>
2015-01-13 00:59:23 -0300

    threshold can specify center for divergent scales

--- matta/scales.py
+++ matta/scales.py
@@ -3,13 +3,21 @@ import seaborn as sns
 import scipy
 from matplotlib.colors import rgb2hex
 
-def threshold_scale(values, palette, is_domain=False, extend_by=0.05):
+def threshold_scale(values, palette, is_domain=False, extend_by=0.05, center=None):
     if not is_domain:
-        color_domain = np.linspace(values.min(), values.max(), len(palette) - 1)
+        if center is None:
+            color_domain = np.linspace(values.min(), values.max(), len(palette) - 1)
+        else:
+            diff = max(values.max() - center, center - values.min())
+            color_domain = np.linspace(center - diff, center + diff, len(palette) - 1)
     else:
-        color_domain = np.linspace(values[0], values[1], len(palette) - 1)
+        if center is None:
+            color_domain = np.linspace(values[0], values[1], len(palette) - 1)
+        else:
+            diff = max(values[1] - center, center - values[0])
+            color_domain = np.linspace(center - diff, center + diff, len(palette) - 1)
 
-    extension = (values.max() - values.min()) * extend_by
+    extension = (color_domain.max() - color_domain.min()) * extend_by
 
     color_scale = {
         'range': map(rgb2hex, palette),
