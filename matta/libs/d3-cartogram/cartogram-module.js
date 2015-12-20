c566f4e09185f227a2c9e05b44158a23c072bdc1
carnby <eduardo.graells@gmail.com>
2015-12-20 17:19:18 -0300

    removing unused file

--- matta/libs/d3-cartogram/cartogram-module.js
+++
@@ -1,9 +0,0 @@
-define(['d3', './cartogram'],
-    function(d3, c) {
-      "use strict";
-      console.log('in cartogram module', d3, c);
-      var cartogram = d3.cartogram;
-      console.log(cartogram, d3);
-      //delete d3['cartogram'];
-      return cartogram;
-    });
