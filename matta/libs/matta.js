a150df1e64a1efa85ce8d11303d8d3ec75deaac0
carnby <eduardo.graells@gmail.com>
2016-03-26 11:45:35 -0300

    Added a generate_uuid function to matta.js

--- matta/libs/matta.js
+++ matta/libs/matta.js
@@ -146,5 +146,23 @@ define('matta', ['d3', 'legend'], function(d3, legend) {
         return hours + ':' + minutes + ':' + seconds;
     };
 
+    matta.generate_uuid = function() {
+        /**
+         * Generates a random unique string.
+         *
+         * Source: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript/8809472#8809472
+         */
+        var d = new Date().getTime();
+        if (window.performance && typeof window.performance.now === "function") {
+            d += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+    };
+
     return matta;
 });
